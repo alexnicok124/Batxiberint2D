a16c2cdeba6d7f5ed35a511b3cec7fe64b8f6e66
Marc Cui <marc.cui@escolaindustrial.org>
2023-08-29 18:36:15 +0200

    Minor changes + beta skin for melee enemy

--- Joc-2D/Assets/Scripts/Player/Stamina.cs
+++ Joc-2D/Assets/Scripts/Player/Stamina.cs
@@ -17,6 +17,8 @@ public class Stamina : MonoBehaviour
 
     void Update()
     {
+        Debug.Log(StaminaPoints);
+
         if(Input.GetKey(KeyCode.F) && StaminaPoints > 30.0f){ //aquí es donde puede correr, tengo que asignar CanRunning del otro script como true
             ReduceStaminaPoints(); //mientras corra no puede regenerar stamina. 
             //enable sprinting
