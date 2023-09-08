cd41ded4933888f2e925f3aed654a88a342a3bcc
Marc Cui <marc.cui@escolaindustrial.org>
2023-09-08 20:34:05 +0200

    Optimization of the win condition code

--- Joc-2D/Assets/Scripts/Colliders.cs
+++ Joc-2D/Assets/Scripts/Colliders.cs
@@ -3,19 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class winMessage : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider collision)
-    {
-        Debug.Log("Hit");
-        if (collision.tag == "Player") 
-        {
-            SceneManager.LoadScene("MenuVictoria");
-        }       
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Hit");
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
             SceneManager.LoadScene("MenuVictoria");
         }
